670225646fb8c86d15db3a34fe258648bc911997
DmitrySkripkin <sepia13@yandex.ru>
2021-03-26 12:25:24 +0300

    Maybe I just pushed files I had to ignore

--- android/src/main/java/com/hubhead/plugin/HHPlugin.java
+++ android/src/main/java/com/hubhead/plugin/HHPlugin.java
@@ -1,20 +1,41 @@
 package com.hubhead.plugin;
 
+import android.R;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import android.app.NotificationChannel;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+
+import java.util.jar.Manifest;
 
 @NativePlugin
 public class HHPlugin extends Plugin {
 
     @PluginMethod
     public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", value);
-        call.success(ret);
+        String body = call.getString("body");
+        String title = call.getString("title");
+        String tag = call.getString("tag");
+        Notification notification = new NotificationCompat.Builder(getContext(), "0")
+                    .setContentTitle(title)
+                    .setContentText(body)
+                    .setSmallIcon(getContext().getApplicationInfo().icon)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                    .build();
+            NotificationManagerCompat manager = NotificationManagerCompat.from(getContext());
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                NotificationChannel channel = new NotificationChannel(
+                        "0", "0", NotificationManager.IMPORTANCE_DEFAULT);
+                manager.createNotificationChannel(channel);
+            }
+            manager.notify(tag, 0, notification);
     }
 }
