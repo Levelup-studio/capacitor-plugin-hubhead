d93dfbf5913aaaec4d1ee7d7f65b37425f68c309
DmitrySkripkin <sepia13@yandex.ru>
2021-03-29 15:49:06 +0300

    iOS mvp plugin

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,19 +1,24 @@
 import Foundation
 import Capacitor
-
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(HHPlugin)
 public class HHPlugin: CAPPlugin {
-
     @objc func notify(_ call: CAPPluginCall) {
         let content = UNMutableNotificationContent()
-        content.title = "Pupa"
-        content.body = "And Lupa"
-        let uuidString = UUID().uuidString
-        let request = UNNotificationRequest(identifier: uuidString,
+        let body = call.getString("body") ?? ""
+        let title = call.getString("title") ?? ""
+        let data = call.getObject("data") ?? [:]
+        let tag = call.getString("tag") ?? ""
+        content.title = title
+        content.body = body
+        content.userInfo = data
+        content.threadIdentifier = tag
+        var uuidString = UUID().uuidString
+        var request = UNNotificationRequest(identifier: uuidString,
                     content: content, trigger: nil)
+        var notificationCenter = UNUserNotificationCenter.current().add(request)
     }
 }
